36ead383b031d39dade182068698cce6a1939fe7
e-yi <e_yi@foxmail.com>
2019-10-23 15:07:41 +0800

    fix a bug

--- model.py
+++ model.py
@@ -72,9 +72,9 @@ class NSTrainSet(Dataset):
     完全随机的负采样 todo 改进一下？
     """
 
-    def __init__(self, sample, path_size, neg=5):
+    def __init__(self, sample, node_size, neg=5):
         """
-        :param path_size: 元路径总数
+        :param node_size: 节点数目
         :param neg: 负采样数目
         :param sample: HIN.sample()返回值，(start_node, end_node, path_id)
         """
@@ -88,7 +88,7 @@ class NSTrainSet(Dataset):
         y[:l] = 1
 
         # x[l:, 2] = np.random.randint(0, path_size - 1, (l * neg,))
-        x[l:, 1] = np.random.randint(0, path_size - 1, (l * neg,))
+        x[l:, 1] = np.random.randint(0, node_size - 1, (l * neg,))
 
         self.x = torch.LongTensor(x)
         self.y = torch.FloatTensor(y)
@@ -102,66 +102,3 @@ class NSTrainSet(Dataset):
     def __len__(self):
         return self.length
 
-
-if __name__ == '__main__':
-    import pandas as pd
-    import torch.optim as optim
-
-    from walker import load_a_HIN_from_pandas
-
-    # set method parameters
-    window = 4
-    walk = 10
-    walk_length = 300
-    embed_size = 100
-    neg = 5
-    sigmoid_reg = False
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f'device = {device}')
-
-    # set dataset [PLEASE USE YOUR OWN DATASET TO REPLACE THIS]
-    demo_edge = pd.read_csv('./demo_data.csv', index_col=0)
-
-    edges = [demo_edge]
-
-    print('finish loading edges')
-
-    # init HIN
-    hin = load_a_HIN_from_pandas(edges)
-    hin.window = window
-
-    dataset = NSTrainSet(hin.sample(walk_length, walk), hin.path_size, neg=neg)
-
-    hin2vec = HIN2vec(hin.node_size, hin.path_size, embed_size, sigmoid_reg)
-
-    # load model
-    # hin2vec.load_state_dict(torch.load('hin2vec.pt'))
-
-    # set training parameters
-    n_epoch = 10
-    batch_size = 20
-    log_interval = 200
-
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    optimizer = optim.Adam(hin2vec.parameters())  # 原作者使用的是SGD？ 这里使用Adam
-    loss_function = nn.BCELoss()
-
-    for epoch in range(n_epoch):
-        train(log_interval, hin2vec, device, data_loader, optimizer, loss_function, epoch)
-
-    # set output file
-    node_vec_fname = 'node_vec.txt'
-    # path_vec_fname = 'meta_path_vec.txt'
-    path_vec_fname = None
-
-    print(f'saving node embedding vectors to {node_vec_fname}...')
-    node_embeds = pd.DataFrame(hin2vec.start_embeds.weight.data.numpy())
-    node_embeds.rename(hin.id2node).to_csv(node_vec_fname, sep=' ')
-
-    if path_vec_fname:
-        print(f'saving meta path embedding vectors to {path_vec_fname}...')
-        path_embeds = pd.DataFrame(hin2vec.path_embeds.weight.data.numpy())
-        path_embeds.rename(hin.id2path).to_csv(path_vec_fname, sep=' ')
-
-    # save model
-    # torch.save(hin2vec.state_dict(), 'hin2vec.pt')
